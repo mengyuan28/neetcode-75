64a2ae2c49907a6918ad3041d19814a344c307ac
mengyuan28 <mengyuan28@ucla.edu>
2026-05-03 16:51:04 -0700

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 0 or n == 0:
+            return 0
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+        for i in range(0, m):
+            for j in range(0, n):
+                if i > 0:
+                    dp[i][j] += dp[i-1][j]
+                if j > 0:
+                    dp[i][j] += dp[i][j-1]
+        print(dp)
+        return dp[m-1][n-1]
